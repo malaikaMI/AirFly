b735da58ec738ff40fd8c12263b8b78c11021136
malaikaMI <malaikamic@gmail.com>
2018-11-05 11:34:38 -0500

    Added some features

--- config/application.rb
+++ config/application.rb
@@ -3,12 +3,10 @@ require "rails/all"
 
 Bundler.require(*Rails.groups)
 
-module Balu
+module AirFly
   class Application < Rails::Application
     config.generators.test_framework = :rspec
     config.generators.fixture_replacement :factory_girl
     config.active_record.raise_in_transactional_callbacks = true
-    # config/application.rb
-    config.assets.initialize_on_precompile = false
   end
 end
